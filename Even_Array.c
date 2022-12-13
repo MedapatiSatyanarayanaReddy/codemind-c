1c17894eeab27373ca8efb6b84d275b8f6e84e12
MedapatiSatyanarayanaReddy <119839777+MedapatiSatyanarayanaReddy@users.noreply.github.com>
2022-12-13 10:54:04 +0530

    #include<stdio.h> int main() {     int n,i,cnt=0;     scanf("%d",&n);     int a[n];     for(i=0;i<n;i++)     {         scanf("%d",&a[i]);     }     for(i=0;i<n;i++)     {         if(a[i]%2==0)         {             cnt++;         }     }         if(cnt==n)         {             printf("True");         }         else         {             printf("False");         }
    }

---
+++ Even_Array.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+int main()
+{
+    int n,i,cnt=0;
+    scanf("%d",&n);
+    int a[n];
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+        if(a[i]%2==0)
+        {
+            cnt++;
+        }
+    }
+        if(cnt==n)
+        {
+            printf("True");
+        }
+        else
+        {
+            printf("False");
+        }
+    
+}
