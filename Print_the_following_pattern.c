fea51efb426d6ac4c810454cc3110d0a8fc4cfb9
MedapatiSatyanarayanaReddy <119839777+MedapatiSatyanarayanaReddy@users.noreply.github.com>
2023-01-23 20:42:39 +0530

    #include<stdio.h> int main() {     int n,c=0-1;     scanf("%d",&n);     for(int i=n;i>=1;i--)     {         c++;         for(int j=1;j<=i;j++)         {             printf("%d ",j+c);             }             printf(" ");             } }

---
+++ Print_the_following_pattern.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+    int n,c=0-1;    
+    scanf("%d",&n);
+    for(int i=n;i>=1;i--)
+    {
+        c++;        
+        for(int j=1;j<=i;j++)
+        {
+            printf("%d ",j+c); 
+            }
+            printf("
+");  
+            }
+}
+
