fee87c4d811e7af4832799287c8a679f14389ab4
MedapatiSatyanarayanaReddy <119839777+MedapatiSatyanarayanaReddy@users.noreply.github.com>
2023-05-22 10:29:45 +0530

    #include<stdio.h> int main() {     int n,m,i,j,s=0;     scanf("%d %d",&n,&m);     int a[n][m];     for(i=0;i<n;i++)     {         for(j=0;j<m;j++)         {             scanf("%d",&a[i][j]);             s+=a[i][j];         }     }     printf("%d",s);     return 0; }

---
+++ Sum_of_matrix_elements.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+    int n,m,i,j,s=0;
+    scanf("%d
+%d",&n,&m);
+    int a[n][m];
+    for(i=0;i<n;i++)
+    {
+        for(j=0;j<m;j++)
+        {
+            scanf("%d",&a[i][j]);
+            s+=a[i][j];
+        }
+    }
+    printf("%d",s);
+    return 0;
+}
