fb9920a086f44945872491436fdff473fb1e7d6e
MedapatiSatyanarayanaReddy <119839777+MedapatiSatyanarayanaReddy@users.noreply.github.com>
2023-05-22 10:27:57 +0530

    #include<stdio.h> int main() {     int n,i;     scanf("%d",&n);     long long int a[n],sum=0;     for(i=0;i<n;i++)     {         scanf("%lld",&a[i]);         sum+=a[i];     }     printf("%lld",sum); }

---
+++ A_Very_Big_Sum.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+    int n,i;
+    scanf("%d",&n);
+    long long int a[n],sum=0;
+    for(i=0;i<n;i++)
+    {
+        scanf("%lld",&a[i]);
+        sum+=a[i];
+    }
+    printf("%lld",sum);
+}
