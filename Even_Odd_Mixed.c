3c32726cabfe2e1966aae55e2c2e56cd7f7fd2c2
MedapatiSatyanarayanaReddy <119839777+MedapatiSatyanarayanaReddy@users.noreply.github.com>
2023-05-22 10:10:28 +0530

    #include<stdio.h> int main() {     int no,es=0,os=0,c=0,rm;     scanf("%d",&no);     while(no!=0)     {         rm=no%10;         c++;         if(rm%2==0)           es++;         else if(rm%2!=0)            os++;         no/=10;     }     if(es==c)       printf("Even");     else if(os==c)       printf("Odd");     else       printf("Mixed");
    }

--- Even_Odd_Mixed.c
+++ Even_Odd_Mixed.c
@@ -1,32 +1,23 @@
 #include<stdio.h>
 int main()
 {
-    int n,c=0,rem,ec=0,oc=0;
-    scanf("%d",&n);
-    while(n>0)
+    int no,es=0,os=0,c=0,rm;
+    scanf("%d",&no);
+    while(no!=0)
     {
-        rem=n%10;
-        c=c+1;
-        if(rem%2==0)
-        {
-            ec=ec+1;
-        }
-        else if(rem%2!=0)
-        {
-            oc=oc+1;
-        }
-        n=n/10;
+        rm=no%10;
+        c++;
+        if(rm%2==0)
+          es++;
+        else if(rm%2!=0)
+           os++;
+        no/=10;
     }
-    if(c==ec)
-    {
-        printf("Even");
-    }
-    else if(c==oc)
-    {
-        printf("Odd");
-    }
-    else
-    {
-        printf("Mixed");
-    }
+    if(es==c)
+      printf("Even");
+    else if(os==c)
+      printf("Odd");
+    else 
+      printf("Mixed");
+      
 }
